e9630f23cd7cf12a21a50563a8c42b6e2a60a1e6
JXC <153008577+JohnxCoffa@users.noreply.github.com>
2023-12-05 19:10:39 +0100

    upload exercise folder
    exercise 10 included

---
+++ exercises/exercise-10.cs
@@ -0,0 +1,18 @@
+﻿class Exercise10
+{
+    static void Main()
+    {
+        int[] numbers = new int[3];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine($"Enter number {i + 1}");
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int result1 = (numbers[0] + numbers[1]) * numbers[2];
+        int result2 = (numbers[0] * numbers[1]) + (numbers[1] * numbers[2]);
+        Console.WriteLine($"(x + y) * z = {result1}");
+        Console.WriteLine($"(x * y) + (y * z) {result2}");
+    }
+}
