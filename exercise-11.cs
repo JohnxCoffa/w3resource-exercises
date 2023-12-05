6ff1343ed32476594561fb92d79e08f4b4418aa0
JXC <153008577+JohnxCoffa@users.noreply.github.com>
2023-12-05 19:13:08 +0100

    Delete exercise-11.cs

--- exercise-11.cs
+++
@@ -1,9 +0,0 @@
-﻿class Exercise11
-{
-    static void Main()
-    {
-        Console.WriteLine("Enter your age:");
-        int? age = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine($"You look older than {age}");
-    }
-}
